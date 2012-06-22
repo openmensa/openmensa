7cb71abff7bd8851f802be15592183ac9ae55d2e
Jan Graichen <jan.graichen@altimos.de>
2012-06-22 14:02:56 +0200

    Fix authentication specs.

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,47 +1,33 @@
 # encoding: UTF-8
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + "/../spec_helper"
 
 describe "Authentication" do
   describe "Login" do
-    it "should login user with valid credentials" do
-      @identity = FactoryGirl.create(:identity)
-
+    it "should login using twitter" do
       visit root_path
+      click_link "Anmelden"
+      click_link "Twitter"
 
-      click_link 'Log in'
-
-      within 'form#login' do
-        fill_in      "Login",    with: @identity.uid
-        fill_in      "Password", with: @identity.password
-        click_button "Log in"
-      end
-
-      page.should have_content("logged in")
+      page.should have_content("Bob Example")
     end
 
-    it "should return to login form and show error when invalid credentials" do
-      visit login_path
-      within 'form#login' do
-        fill_in      "Login",    with: 'wrong-id'
-        fill_in      "Password", with: 'wrong-secret'
-        click_button "Log in"
-      end
+    it "should login using github" do
+      visit root_path
+      click_link "Anmelden"
+      click_link "GitHub"
 
-      page.should have_content("Invalid login")
+      page.should have_content("Bob Example")
     end
   end
 
   describe "Logout" do
     before :each do
-      @identity = FactoryGirl.create(:identity)
-      login @identity
+      login FactoryGirl.create(:identity)
     end
 
     it "should sign off a user if signed in" do
       visit root_path
-
-      click_link @identity.user.name
-      click_link "Log out"
+      click_link "Abmelden"
 
       User.current.should == User.anonymous
     end
