089d1b1f55acba587522b5c635b351a96d829b71
Jan Graichen <jan.graichen@altimos.de>
2012-07-04 19:14:41 +0200

    fix

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,8 @@ module Openmensa
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.omniauth_services = []
   end
 end
