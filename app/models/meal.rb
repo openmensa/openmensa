dbf3b7ea27c87c5d63eebf0ce2e45ade5e15ba57
Jan Graichen <jan.graichen@altimos.de>
2012-07-20 15:16:32 +0200

    Style canteen view page

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,6 +5,8 @@ class Meal < ActiveRecord::Base
   attr_accessible :date, :description, :name, :category, :canteen_id, :canteen
   validates :date, :name, :category, :canteen_id, presence: true
 
+  scope :today, lambda { where(date: Time.zone.now.to_date) }
+
   def date=(date)
     write_attribute :date, date.to_date unless date.nil?
   end
