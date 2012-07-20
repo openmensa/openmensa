9ca6e92f0206656c686cb8d09595b8c7bdcb80fb
Jan Graichen <jan.graichen@altimos.de>
2012-07-20 12:21:26 +0200

    Add OpenLayers stylesheet to precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Openmensa::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( screen.css ie.css print.css )
+  config.assets.precompile += %w( screen.css ie.css print.css theme/default/style.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
