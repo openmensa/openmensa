5db6efa1e4da6fe7c6ed560a542f154efccf3924
Jan Graichen <jan.graichen@altimos.de>
2012-07-31 17:05:06 +0200

    Add meal scopes

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,7 +5,9 @@ class Meal < ActiveRecord::Base
   attr_accessible :date, :description, :name, :category, :canteen_id, :canteen
   validates :date, :name, :category, :canteen_id, presence: true
 
-  scope :today, lambda { where(date: Time.zone.now.to_date) }
+  scope :on, lambda { |date| where(date: date.to_date) }
+  scope :today, lambda { where(date: Time.now.to_date) }
+  scope :tomorrow, lambda { where(date: Time.now.to_date + 1.day) }
 
   def date=(date)
     write_attribute :date, date.to_date unless date.nil?
