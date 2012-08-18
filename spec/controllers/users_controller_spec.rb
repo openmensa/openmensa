f4b52a3637ae991eea4cdc19fe10e08d6e1c6ea9
Jan Graichen <jg@altimos.de>
2012-08-18 17:03:07 +0200

    Add auth spec for users#update

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,12 +2,23 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe UsersController do
+  let(:user) { FactoryGirl.create :user }
+
   describe "#show" do
-    let(:user) { FactoryGirl.create :user }
     before { user }
 
     it "should not allow unauthorized access by anonymous" do
       get :show, id: user.id
+
+      response.status.should == 401
+    end
+  end
+
+  describe "#update" do
+    before { user }
+
+    it "should not be accessible by anonymous" do
+      put :update, id: user.id, user_name: 'Bobby'
 
       response.status.should == 401
     end
