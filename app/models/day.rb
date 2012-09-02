33864f9d570cdbbd70e8515f176068ad7ae30bd1
Jan Graichen <jg@altimos.de>
2012-09-02 23:11:39 +0200

    Remove old scopes from meal.

--- app/models/day.rb
+++ app/models/day.rb
@@ -5,10 +5,6 @@ class Day < ActiveRecord::Base
   attr_accessible :date
   validates :date, :canteen_id, presence: true
 
-  scope :on, lambda { |date| where(date: date.to_date) }
-  scope :today, lambda { where(date: Time.now.to_date) }
-  scope :tomorrow, lambda { where(date: Time.now.to_date + 1.day) }
-
   def date=(date)
     write_attribute :date, date.to_date unless date.nil?
   end
