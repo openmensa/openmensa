33a55eb8af3bdead9c0030db0c9798c34410d656
Jan Graichen <jg@altimos.de>
2013-01-18 23:51:00 +0100

    Fix non-REST mobile endpoint to include meals.

--- app/decorators/day_decorator.rb
+++ app/decorators/day_decorator.rb
@@ -4,9 +4,11 @@ class DayDecorator < Draper::Decorator
   decorates_association :meals
 
   def to_version_2(options)
-    {
+    result = {
       date: model.date.iso8601,
       closed: model.closed
     }
+    result[:meals] = meals if options[:include].include? :meals
+    result
   end
 end
