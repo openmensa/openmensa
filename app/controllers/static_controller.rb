7640c4980642ab9559d891b56ef67cee9bfbe20e
Jan Graichen <jg@altimos.de>
2013-03-08 11:55:25 +0100

    Extract favorite list in  partial and embedd it on canteen show page for quicker navigation.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,12 +3,6 @@ class StaticController < ApplicationController
 
   def index
     page = (params[:id] || 'index').gsub /[^A-z0-9_\-]/, ''
-    @favorites = if current_user.internal?
-      []
-    else
-      current_user.favorites.order('priority')
-    end
-
     render file: 'static/' + page
   end
 end
