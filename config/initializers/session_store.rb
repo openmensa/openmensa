513c92dc4d2061b7524f22996b7b73c09d2ca6ab
Jan Graichen <jg@altimos.de>
2013-05-02 00:10:44 +0200

    Switch back to cookie session store as encrypted store is automatically configured now.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Openmensa::Application.config.session_store :encrypted_cookie_store, key: '_openmensa_session'
+Openmensa::Application.config.session_store :cookie_store, key: '_openmensa_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
