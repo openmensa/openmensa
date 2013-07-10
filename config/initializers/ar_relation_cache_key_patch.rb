f5763966d44952bafa8958d01d608858cee8a09c
Jan Graichen <jg@altimos.de>
2013-07-10 14:39:05 +0200

    Cache index page map data.
    Add cache option to map partial caching map JSON based on canteen collection. Needs patch for AR scope to provide cache_key for collection.

---
+++ config/initializers/ar_relation_cache_key_patch.rb
@@ -0,0 +1,30 @@
+module RelationCacheKey
+  # Compute the cache key of a group of records.
+  #
+  #   Item.cache_key # => "0b27dac757428d88c0f3a0298eb0278f"
+  #   Item.active.cache_key # => "0b27dac757428d88c0f3a0298eb0278e"
+  #
+  def cache_key
+    scope_sql = scoped.select("#{table_name}.id, #{table_name}.updated_at").to_sql
+
+    # PostgreSQL only
+    sql = "SELECT md5(array_agg(id || '-' || updated_at)::text) " +
+        "FROM (#{scope_sql}) as query"
+
+    md5 = connection.select_value(sql)
+
+    key = if md5.present?
+            md5
+          else
+            "empty"
+          end
+
+    "#{model_name.cache_key}/#{key}"
+  end
+
+  def updated_at
+    scoped.order(:updated_at).last.updated_at
+  end
+end
+
+ActiveRecord::Base.extend RelationCacheKey
