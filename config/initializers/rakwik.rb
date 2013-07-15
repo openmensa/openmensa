ca328fff20a5193099b3d65b3d9d9ec917f9b1d4
Jan Graichen <jg@altimos.de>
2013-07-15 19:43:44 +0200

    Allow to start Rakwik in dev mode by enabling RAKWIK evn var.

--- config/initializers/rakwik.rb
+++ config/initializers/rakwik.rb
@@ -1,4 +1,4 @@
-if Rails.env.production?
+if Rails.env.production? or ENV['RAKWIK']
   Rails.application.config.middleware.use Rakwik::Tracker, piwik_url: 'http://dev.altimos.de/piwik/piwik.php',
                                           site_id: '4', token_auth: ENV['RAKWIK_AUTH_TOKEN'], track_404: false,
                                           path: '/api'
