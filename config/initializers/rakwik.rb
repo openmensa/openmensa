9a985cb33d02245cddde39570eb04b95e266a556
Jan Graichen <jg@altimos.de>
2013-07-17 16:44:31 +0200

    Revert "Remove env.rb ENV loading as it does not work reliable. ENV vars are given by exported server conf now."
    This reverts commit 38148c05837cffbf41092ad198a05490d2d393e7.

--- config/initializers/rakwik.rb
+++ config/initializers/rakwik.rb
@@ -1,9 +1,5 @@
 if Rails.env.production? or ENV['RAKWIK']
-  if ENV['RAKWIK_AUTH_TOKEN']
-    Rails.application.config.middleware.use Rakwik::Tracker, piwik_url: 'http://dev.altimos.de/piwik/piwik.php',
-                                            site_id: '4', token_auth: ENV['RAKWIK_AUTH_TOKEN'], track_404: false,
-                                            path: '/api'
-  else
-    Rails.logger.warn 'No RAKWIK_ATH_TOKEN given.'
-  end
+  Rails.application.config.middleware.use Rakwik::Tracker, piwik_url: 'http://dev.altimos.de/piwik/piwik.php',
+                                          site_id: '4', token_auth: ENV['RAKWIK_AUTH_TOKEN'], track_404: false,
+                                          path: '/api'
 end
