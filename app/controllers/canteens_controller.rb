78abdac8704e51ec4cdd08528b47af804f405dcb
Jan Graichen <jg@altimos.de>
2014-07-10 11:33:43 +0200

    Backport patch from develop

--- app/controllers/canteens_controller.rb
+++ app/controllers/canteens_controller.rb
@@ -33,12 +33,12 @@ class CanteensController < ApplicationController
 
   def show
     if params[:date]
-      @date  = Date.parse params[:date]
+      @date  = Date.parse params[:date].to_s
     else
-      @date  = Time.zone.now.to_date
+      @date  = Time.zone.now
     end
 
-    @meals = @canteen.meals.where(date: @date)
+    @meals = @canteen.meals.for(@date)
   end
 
   def fetch
--- spec/controllers/canteens_controller_spec.rb
+++ spec/controllers/canteens_controller_spec.rb
@@ -14,13 +14,13 @@ describe CanteensController do
     it "canteen's meals for today" do
       get :show, id: canteen.id
 
-      assigns(:meals).should == canteen.meals.where(date: Time.zone.now.to_date)
+      assigns(:meals).should == canteen.meals.for(Time.zone.now)
     end
 
     it 'should fetch meals for given date parameter' do
-      get :show, id: canteen.id, date: Time.zone.now.to_date + 1.day
+      get :show, id: canteen.id, date: Time.zone.now + 1.day
 
-      assigns(:meals).should == canteen.meals.where(date: Time.zone.now.to_date + 1.day)
+      assigns(:meals).should == canteen.meals.for(Time.zone.now + 1.day)
     end
   end
 
