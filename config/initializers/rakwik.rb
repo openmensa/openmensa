8c1b2a04480c7ab05725214197fafe7f7c1e8dff
Jan Graichen <jg@altimos.de>
2014-08-17 11:27:05 +0200

    require rackwik only when configured

--- config/initializers/rakwik.rb
+++ config/initializers/rakwik.rb
@@ -1,8 +1,12 @@
 if Rails.env.production? or ENV['RAKWIK']
-  Rails.application.config.middleware.use Rakwik::Tracker, \
-    piwik_url: 'http://dev.altimos.de/piwik/piwik.php',
-    site_id: '4',
-    track_404: false,
-    token_auth: Rails.application.secrets.rakwik_auth_token,
-    path: '/api'
+  if Rails.application.secrets.rakwik_auth_token
+    require 'rakwik'
+
+    Rails.application.config.middleware.use Rakwik::Tracker, \
+      piwik_url: 'http://dev.altimos.de/piwik/piwik.php',
+      site_id: '4',
+      track_404: false,
+      token_auth: Rails.application.secrets.rakwik_auth_token,
+      path: '/api'
+  end
 end
