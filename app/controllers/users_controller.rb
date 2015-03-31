521398d24e4adc7c4dccde51547891a5cf978b31
Jan Graichen <jg@altimos.de>
2015-03-31 16:39:42 +0200

    Drop unnecessary code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,19 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: [:show, :update]
   load_and_authorize_resource
 
   def show
   end
 
   def update
-    if @user.update_attributes user_params
+    if @user.update user_params
       flash_for :user, notice: t('message.profile_saved').html_safe
-      redirect_to user_path(@user)
+      redirect_to @user
     else
       render action: :show
     end
   end
 
   private
-
-  def load_user
-    @user = User.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:name, :email, :send_reports)
