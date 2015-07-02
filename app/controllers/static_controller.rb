8497169f8d8cb3828749750197169326caeb215b
Malte Swart <mswart@devtation.de>
2015-07-02 12:23:11 +0200

    fix specs

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,7 +3,7 @@ class StaticController < ApplicationController
   respond_to :html
 
   def index
-    if request.referer.blank? && @user.logged? && @user.favorites.any?
+    if request.referer.blank? && @user && @user.logged? && @user.favorites.any?
       redirect_to menu_path
     end
   end
