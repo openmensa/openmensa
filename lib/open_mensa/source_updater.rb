dbfdc6291f043644d47171a363c9c750b2b08cd1
Malte Swart <mswart@devtation.de>
2015-07-13 10:39:17 +0200

    source_updater: retry field is optional

--- lib/open_mensa/source_updater.rb
+++ lib/open_mensa/source_updater.rb
@@ -90,7 +90,7 @@ class OpenMensa::SourceUpdater < OpenMensa::BaseUpdater
       when 'source'
         data[:source_url] = element.content
       when 'schedule'
-        data[:retry] = element['retry'].split(' ').map(&:to_i)
+        data[:retry] = element['retry'].split(' ').map(&:to_i) if element.key? 'retry'
         data[:schedule] = [element['minute'] || '0', element['hour'], element['dayOfMonth'],
                            element['month'] || '*', element['dayOfWeek'], ].join(' ')
       end
