285c9efb4fea1d6576bb7ef64b8e2412a3fe9338
Jan Graichen <jg@altimos.de>
2016-01-24 11:42:30 +0100

    Disable rakwik

--- config/initializers/rakwik.rb
+++ config/initializers/rakwik.rb
@@ -1,12 +1,12 @@
 if Rails.env.production? || ENV['RAKWIK']
-  if Rails.application.secrets.rakwik_auth_token
-    require 'rakwik'
+  # if Rails.application.secrets.rakwik_auth_token
+  #   require 'rakwik'
 
-    Rails.application.config.middleware.use Rakwik::Tracker, \
-      piwik_url: 'http://dev.altimos.de/piwik/piwik.php',
-      site_id: '4',
-      track_404: false,
-      token_auth: Rails.application.secrets.rakwik_auth_token,
-      path: '/api'
-  end
+  #   Rails.application.config.middleware.use Rakwik::Tracker, \
+  #     piwik_url: 'http://dev.altimos.de/piwik/piwik.php',
+  #     site_id: '4',
+  #     track_404: false,
+  #     token_auth: Rails.application.secrets.rakwik_auth_token,
+  #     path: '/api'
+  # end
 end
