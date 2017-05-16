1b78aa1d7288e5e75c61c106b318954a1307d304
Jan Graichen <jgraichen@altimos.de>
2017-05-16 19:39:18 +0200

    Add library path to eager load too

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module Openmensa
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths << "#{config.root}/lib"
+    config.eager_load_paths << "#{Rails.root}/lib"
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
