159cf2fd86cc94860ec019597479bd27c6381116
Jan Graichen <jgraichen@altimos.de>
2021-05-09 20:50:21 +0200

    Avoid initialize sentry without DSN
    Otherwise running specs can result in exception when the nil dsn is
    accessed.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.dsn = ENV.fetch("SENTRY_DSN", Rails.application.secrets.sentry_dsn)
-  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+SENTRY_DSN = ENV.fetch("SENTRY_DSN", Rails.application.secrets.sentry_dsn)
 
-  # Do not send full list of gems with each event
-  config.send_modules = false
+if SENTRY_DSN.present?
+  Sentry.init do |config|
+    config.dsn = SENTRY_DSN
+    config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
-  # Set tracesSampleRate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production
-  config.traces_sample_rate = 1.0
+    # Do not send full list of gems with each event
+    config.send_modules = false
 
-  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
-  config.before_send = lambda do |event, _hint|
-    # use Rails' parameter filter to sanitize the event
-    filter.filter(event.to_hash)
+    # Set tracesSampleRate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production
+    config.traces_sample_rate = 1.0
+
+    filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+    config.before_send = lambda do |event, _hint|
+      # use Rails' parameter filter to sanitize the event
+      filter.filter(event.to_hash)
+    end
   end
 end
