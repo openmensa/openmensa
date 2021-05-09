88d8ca23add771bd3edead11020ba9f20f725e70
Jan Graichen <jgraichen@altimos.de>
2021-05-09 18:08:04 +0200

    Use preferred Rails.root.join style

--- config/application.rb
+++ config/application.rb
@@ -26,15 +26,15 @@ module Openmensa
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths << "#{config.root}/lib"
-    config.eager_load_paths << "#{Rails.root}/lib"
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = "Berlin"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join("app", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.load_path += Dir[Rails.root.join("app/locales/**/*.{rb,yml}").to_s]
     config.i18n.default_locale = :de
 
     # Loaded OmniAuth services will be stored here
