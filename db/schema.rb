33a4c74004d0fcb3be0f61aab5ffd3ca636da2b6
Jan Graichen <jgraichen@altimos.de>
2021-12-24 11:49:23 +0100

    chore: Regenerate schema from fresh database

--- db/schema.rb
+++ db/schema.rb
@@ -16,35 +16,35 @@ ActiveRecord::Schema.define(version: 2021_05_09_203331) do
   enable_extension "plpgsql"
 
   create_table "canteens", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "address", limit: 255
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "last_fetched_at"
     t.float "longitude"
     t.float "latitude"
-    t.string "city", limit: 255
-    t.string "state", limit: 255, default: "new", null: false
-    t.string "phone", limit: 255
-    t.string "email", limit: 255
+    t.string "city"
+    t.string "state", default: "new", null: false
+    t.string "phone"
+    t.string "email"
     t.boolean "availibility", default: true
-    t.string "openingTimes", limit: 255, array: true
+    t.string "openingTimes", array: true
     t.integer "replaced_by"
   end
 
   create_table "data_proposals", id: :serial, force: :cascade do |t|
     t.integer "canteen_id"
     t.integer "user_id"
-    t.string "state", limit: 255, default: "new", null: false
-    t.string "name", limit: 255
-    t.string "city", limit: 255
-    t.string "address", limit: 255
+    t.string "state", default: "new", null: false
+    t.string "name"
+    t.string "city"
+    t.string "address"
     t.float "longitude"
     t.float "latitude"
-    t.string "phone", limit: 255
-    t.string "email", limit: 255
-    t.string "availibility", limit: 255
-    t.string "openingTimes", limit: 255, array: true
+    t.string "phone"
+    t.string "email"
+    t.string "availibility"
+    t.string "openingTimes", array: true
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -53,8 +53,8 @@ ActiveRecord::Schema.define(version: 2021_05_09_203331) do
     t.integer "canteen_id"
     t.date "date"
     t.boolean "closed", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["canteen_id"], name: "index_days_on_canteen_id"
   end
 
@@ -62,15 +62,15 @@ ActiveRecord::Schema.define(version: 2021_05_09_203331) do
     t.integer "canteen_id"
     t.integer "user_id"
     t.integer "priority"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "feed_fetches", id: :serial, force: :cascade do |t|
     t.integer "feed_id"
-    t.string "state", limit: 255, null: false
-    t.string "reason", limit: 255, null: false
-    t.string "version", limit: 255
+    t.string "state", null: false
+    t.string "reason", null: false
+    t.string "version"
     t.integer "added_days"
     t.integer "updated_days"
     t.integer "added_meals"
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 2021_05_09_203331) do
   create_table "feedbacks", id: :serial, force: :cascade do |t|
     t.integer "canteen_id"
     t.integer "user_id"
-    t.string "state", limit: 255, default: "new", null: false
+    t.string "state", default: "new", null: false
     t.text "message"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -93,11 +93,11 @@ ActiveRecord::Schema.define(version: 2021_05_09_203331) do
   create_table "feeds", id: :serial, force: :cascade do |t|
     t.integer "source_id"
     t.integer "priority", default: 0, null: false
-    t.string "name", limit: 255, null: false
-    t.string "url", limit: 255, null: false
-    t.string "schedule", limit: 255
+    t.string "name", null: false
+    t.string "url", null: false
+    t.string "schedule"
     t.integer "retry", array: true
-    t.string "source_url", limit: 255
+    t.string "source_url"
     t.datetime "last_fetched_at"
     t.datetime "next_fetch_at"
     t.integer "current_retry", array: true
@@ -108,21 +108,21 @@ ActiveRecord::Schema.define(version: 2021_05_09_203331) do
 
   create_table "identities", id: :serial, force: :cascade do |t|
     t.integer "user_id"
-    t.string "provider", limit: 255
-    t.string "uid", limit: 255
-    t.string "token", limit: 255
-    t.string "secret", limit: 255
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "token"
+    t.string "secret"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
   create_table "meals", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "description", limit: 255
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "category", limit: 255
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "category"
     t.integer "day_id"
     t.decimal "price_student", precision: 8, scale: 2
     t.decimal "price_employee", precision: 8, scale: 2
@@ -141,31 +141,31 @@ ActiveRecord::Schema.define(version: 2021_05_09_203331) do
 
   create_table "messages", id: :serial, force: :cascade do |t|
     t.integer "canteen_id"
-    t.string "type", limit: 255, null: false
-    t.string "priority", limit: 255, null: false
+    t.string "type", null: false
+    t.string "priority", null: false
     t.text "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "messageable_type"
     t.integer "messageable_id"
-    t.string "messageable_type", limit: 255
     t.index ["canteen_id"], name: "index_messages_on_canteen_id"
-    t.index ["messageable_id", "messageable_type"], name: "index_messages_on_messageable_id_and_messageable_type"
+    t.index ["messageable_type", "messageable_id"], name: "index_messages_on_messageable"
     t.index ["type"], name: "index_messages_on_type"
   end
 
   create_table "notes", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["name"], name: "index_notes_on_name", unique: true
   end
 
   create_table "parsers", id: :serial, force: :cascade do |t|
     t.integer "user_id"
-    t.string "name", limit: 255, null: false
-    t.string "version", limit: 255
-    t.string "info_url", limit: 255
-    t.string "index_url", limit: 255
+    t.string "name", null: false
+    t.string "version"
+    t.string "info_url"
+    t.string "index_url"
     t.boolean "maintainer_wanted", default: false, null: false
     t.datetime "last_report_at"
     t.datetime "created_at"
@@ -176,8 +176,8 @@ ActiveRecord::Schema.define(version: 2021_05_09_203331) do
   create_table "sources", id: :serial, force: :cascade do |t|
     t.integer "canteen_id"
     t.integer "parser_id"
-    t.string "name", limit: 255, null: false
-    t.string "meta_url", limit: 255
+    t.string "name", null: false
+    t.string "meta_url"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["canteen_id", "parser_id"], name: "index_sources_on_canteen_id_and_parser_id", unique: true
@@ -185,42 +185,33 @@ ActiveRecord::Schema.define(version: 2021_05_09_203331) do
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "email", limit: 255
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "time_zone", limit: 255
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "time_zone"
     t.string "language", limit: 2
-    t.string "login", limit: 255
+    t.string "login"
     t.boolean "admin"
     t.boolean "developer", default: false
     t.datetime "last_report_at"
-    t.string "public_email", limit: 255
-    t.string "public_name", limit: 255
-    t.string "notify_email", limit: 255
-    t.string "info_url", limit: 255
+    t.string "public_email"
+    t.string "public_name"
+    t.string "notify_email"
+    t.string "info_url"
   end
 
   add_foreign_key "canteens", "canteens", column: "replaced_by"
-  add_foreign_key "canteens", "canteens", column: "replaced_by", name: "fk_rails_canteen_replaced_by", on_delete: :restrict
   add_foreign_key "data_proposals", "canteens", on_delete: :cascade
   add_foreign_key "data_proposals", "users", on_delete: :restrict
   add_foreign_key "days", "canteens"
-  add_foreign_key "days", "canteens", name: "fk_rails_days_canteen_id", on_delete: :restrict
   add_foreign_key "favorites", "canteens", on_delete: :cascade
   add_foreign_key "favorites", "users", on_delete: :cascade
-  add_foreign_key "feed_fetches", "feeds", name: "fk_rails_feed_fetches_feed_id", on_delete: :cascade
   add_foreign_key "feed_fetches", "feeds", on_delete: :cascade
   add_foreign_key "feedbacks", "canteens"
-  add_foreign_key "feedbacks", "canteens", name: "fk_rails_feedbacks_canteen_id", on_delete: :restrict
-  add_foreign_key "feeds", "sources", name: "fk_rails_feeds_source_id", on_delete: :cascade
   add_foreign_key "feeds", "sources", on_delete: :cascade
   add_foreign_key "meals", "days"
-  add_foreign_key "meals", "days", name: "fk_rails_meals_day_id", on_delete: :restrict
   add_foreign_key "parsers", "users"
-  add_foreign_key "parsers", "users", name: "fk_rails_parsers_user_id", on_delete: :restrict
-  add_foreign_key "sources", "canteens", name: "fk_rails_canteen_sources"
   add_foreign_key "sources", "canteens", on_delete: :cascade
-  add_foreign_key "sources", "parsers", name: "fk_rails_sources_parser"
   add_foreign_key "sources", "parsers", on_delete: :cascade
 end
