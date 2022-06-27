f9823e7eaf1484aebcce494d25fffcd88c59d958
Jan Graichen <jgraichen@altimos.de>
2022-06-27 08:20:40 +0200

    chore: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_203631) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_27_061228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
