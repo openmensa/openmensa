fd21c1fc6eed22a055e12f2a6833a4047e824576
Jan Graichen <jgraichen@altimos.de>
2022-09-05 12:02:38 +0200

    feat: Mark emails as auto-generated

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,14 @@
 
 class ApplicationMailer < ActionMailer::Base
   default from: "mail@openmensa.org"
+
+  before_action do
+    # https://www.arp242.net/autoreply.html
+
+    # RFC 3834
+    headers['Auto-Submitted'] = 'auto-generated'
+
+    # Microsoft
+    headers['X-Auto-Response-Suppress'] = 'All'
+  end
 end
