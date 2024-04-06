bb3debe4cae7ce8c7916b32834d4c585adb6e840
Jan Graichen <jgraichen@altimos.de>
2024-04-06 16:31:48 +0200

    fix: Remove timestamp from prod log
    The formatter added extra timestamp and log level to STDOUT, which isn't
    needed when messages are added to the journal/syslog.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,6 @@ Rails.application.configure do
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new($stdout)
-    .tap  {|logger| logger.formatter = Logger::Formatter.new }
     .then {|logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
