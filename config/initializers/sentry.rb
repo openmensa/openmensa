d73aff2066deec619f079382a6646147678e3505
Jan Graichen <jgraichen@altimos.de>
2024-04-27 15:23:26 +0200

    chore: Enable profile sampling

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -10,10 +10,10 @@ if SENTRY_DSN.present?
     # Do not send full list of gems with each event
     config.send_modules = false
 
-    # Set tracesSampleRate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production
+    # Set sampling rates to 1.0 to capture 100% of transactions and
+    # profiles for performance monitoring.
     config.traces_sample_rate = 1.0
+    config.profiles_sample_rate = 1.0
 
     filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
     config.before_send = lambda do |event, _hint|
