9a32c1db0fe827e3e96dc8334a241dec388c4bca
Jan Graichen <jgraichen@altimos.de>
2024-05-06 23:34:06 +0200

    feat: Add assets precompression
    Service precompressed assets can speed up loading the page. We only have
    gzip files as of now, but compressed with zopfli.

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,52 +1,79 @@
+import zopfli from "@gfx/zopfli";
 import { rspack } from "@rspack/core";
+import CompressionPlugin from "compression-webpack-plugin";
 import * as path from "node:path";
 import { RspackManifestPlugin } from "rspack-manifest-plugin";
 
-export default {
-  entry: {
-    application: [
-      "./app/javascripts/application.mjs",
-      "./app/javascripts/application.scss",
+export default async (env, argv) => {
+  const production =
+    argv.mode === "production" ||
+    process.env.NODE_ENV === "production" ||
+    process.env.RAILS_ENV === "production";
+
+  const config = {
+    entry: {
+      application: [
+        "./app/javascripts/application.mjs",
+        "./app/javascripts/application.scss",
+      ],
+    },
+    output: {
+      path: path.resolve("public/assets/packed"),
+      publicPath: "/assets/packed/",
+      filename: "[name].[contenthash].js",
+      assetModuleFilename: "static/[name].[contenthash][ext]",
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g|svg)$/,
+          type: "asset",
+        },
+        {
+          test: /\.(sass|scss)$/,
+          use: ["sass-loader"],
+          type: "css/auto",
+        },
+      ],
+    },
+    plugins: [
+      // Inject jQuery into jquery.autocomplete (and application.mjs)
+      new rspack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+      }),
+      // A manifest is required for the Rails application to generate
+      // correct <script> and <link> tags.
+      new RspackManifestPlugin({
+        fileName: ".manifest.json",
+        writeToFileEmit: true,
+      }),
     ],
-  },
-  output: {
-    path: path.resolve("public/assets/packed"),
-    publicPath: "/assets/packed/",
-    filename: "[name].[contenthash].js",
-    assetModuleFilename: "static/[name].[contenthash][ext]",
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|svg)$/,
-        type: "asset",
-      },
-      {
-        test: /\.(sass|scss)$/,
-        use: ["sass-loader"],
-        type: "css/auto",
-      },
-    ],
-  },
-  plugins: [
-    // Inject jQuery into jquery.autocomplete (and application.mjs)
-    new rspack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-    }),
-    // A manifest is required for the Rails application to generate
-    // correct <script> and <link> tags.
-    new RspackManifestPlugin({
-      fileName: ".manifest.json",
-      writeToFileEmit: true,
-    }),
-  ],
-  devServer: {
-    proxy: {
-      "/": {
-        target: "http://localhost:3000",
+    devServer: {
+      proxy: {
+        "/": {
+          target: "http://localhost:3000",
+        },
       },
     },
-  },
+  };
+
+  if (production) {
+    config.plugins.push(
+      new CompressionPlugin({
+        filename: "[path][base].gz",
+        compressionOptions: {
+          numiterations: 15,
+        },
+        algorithm(input, compressionOptions, callback) {
+          return zopfli.gzip(input, compressionOptions, callback);
+        },
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    );
+  }
+
+  return config;
 };
