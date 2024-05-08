634e14ef3f05d3065ca32f83107ef32ba347a492
Jan Graichen <jgraichen@altimos.de>
2024-05-08 13:10:23 +0200

    chore: Update missing good_job database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_06_190349) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_08_110954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -133,6 +133,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_06_190349) do
     t.datetime "finished_at"
     t.text "error"
     t.integer "error_event", limit: 2
+    t.text "error_backtrace", array: true
     t.index ["active_job_id", "created_at"], name: "index_good_job_executions_on_active_job_id_and_created_at"
   end
 
