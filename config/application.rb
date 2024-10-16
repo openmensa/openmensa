35b10d3a780c0813928251d3b64ff604bbe05f9f
Jan Graichen <jgraichen@altimos.de>
2024-10-16 20:06:43 +0200

    fix: broken omniauth login after being redirect back to app
    Only when loading the 7.2 defaults, when being redirected to the app
    after on oauth2 login, the omniauth-oauth2 gems errors out with a
    csrf_detected error.
    
    I'll need to investigate that, but for now only load 7.1 defaults so
    that logins work again.

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Openmensa
 
     # Initialize configuration defaults for originally generated Rails
     # version.
-    config.load_defaults 7.2
+    config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories
     # that do not contain `.rb` files, or that should not be reloaded or
